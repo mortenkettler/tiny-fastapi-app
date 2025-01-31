9140638782cdedadeacedcd8826ffa42ba486e18
mortenkettler <mortenkettler@hotmail.de>
2025-01-31 23:04:03 +0100

    minimal example

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Morten!"}
+
+@app.get("/{name}")
+def read_root(name: str):
+    return {"message": f"Hello, {name}!"}
+
+        
